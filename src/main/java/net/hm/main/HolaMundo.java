1649537bd25a768cbfda87136f842a9b1eac9688
Renzo <renzo.gnu@gmail.com>
2023-11-15 21:42:41 -0500

    Version

--- src/main/java/net/hm/main/HolaMundo.java
+++ src/main/java/net/hm/main/HolaMundo.java
@@ -5,5 +5,6 @@ public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("GITHUB ACTION...!");
+        System.out.println("NETBEANS!");
     }
 }
